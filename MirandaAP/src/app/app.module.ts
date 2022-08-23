2127f073e634007ca722c16fd02e0afc4dea8f61
Miranda Frias <mirifv.96@gmail.com>
2022-08-23 04:45:34 -0300

    23/08-4:25
    progreso

--- MirandaAP/src/app/app.module.ts
+++ MirandaAP/src/app/app.module.ts
@@ -8,7 +8,10 @@ import { BannerComponent } from './components/banner/banner.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+// Import ng-circle-progress
+import { NgCircleProgressModule } from 'ng-circle-progress'; 
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,11 +19,15 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     BannerComponent,
     LogoAPComponent,
     RedesSocialesComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+
   ],
   providers: [],
   bootstrap: [AppComponent]
