b881dc90763286770d588fd2109ea996a6f0b272
Miranda Frias <mirifv.96@gmail.com>
2022-09-17 01:44:00 -0300

    17/09

--- MirandaAP/src/app/app.module.ts
+++ MirandaAP/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
+
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
@@ -21,7 +21,6 @@ import { FooterComponent } from './components/footer/footer.component';
     HeaderComponent,
     BannerComponent,
     LogoAPComponent,
-    RedesSocialesComponent,
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
