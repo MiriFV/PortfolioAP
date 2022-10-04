b09b77ec56d014a0bde051b948ba912709b2c4b0
Miranda Frias <mirifv.96@gmail.com>
2022-10-04 18:37:24 -0300

    agrego back

---
+++ MirandaAP - Backend/src/main/java/com/portfolio/mirifv/MirifvApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mirifv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirifvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirifvApplication.class, args);
+	}
+
+}
---
+++ MirandaAP - Backend/src/test/java/com/portfolio/mirifv/MirifvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mirifv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirifvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
