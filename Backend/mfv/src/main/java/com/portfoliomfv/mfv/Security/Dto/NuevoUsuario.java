ff48aa129b071b69335b68d039c9d9f278b1761b
Miranda Frias <mirifv.96@gmail.com>
2022-10-23 04:06:16 -0300

    23/10

---
+++ Backend/mfv/src/main/java/com/portfoliomfv/mfv/Security/Dto/NuevoUsuario.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfoliomfv.mfv.Security.Dto;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.validation.constraints.NotBlank;
+
+
+public class NuevoUsuario {
+    @NotBlank
+    private String nombreUsuario;
+    private String password;
+    private String nombre;
+    private String email;
+    private Set<String> roles = new HashSet<>();
+    //Get&Set
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+    
+    
+}
